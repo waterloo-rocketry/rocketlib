1ca7b9a160bc0a442ecd3f846d9db965aca9dd85
Roberto Valenzuela <valenzuelarober@gmail.com>
2024-01-04 12:01:35 -0500

    i2c: create files for i2c driver

---
+++ src/i2c.c
@@ -0,0 +1,9 @@
+#include "i2c.h"
+
+#include "stdio.h"
+
+
+int main()
+{
+    printf("I2C Controller Driver for PIC18F26K83");
+}
---
+++ src/i2c.h
@@ -0,0 +1,4 @@
+#ifndef I2C_H_
+#define I2C_H_
+
+#endif  // I2C_H_
