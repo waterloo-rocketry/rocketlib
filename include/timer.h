0eab49f6a70749b90e51822d0ec96d48b7c86b9c
Mehak Khan <m225khan@uwaterloo.ca>
2024-09-24 18:40:47 -0400

    Documentation for timer millis function (Closes #18)

--- include/timer.h
+++ include/timer.h
@@ -3,14 +3,23 @@
 
 #include <stdint.h>
 
-// utilities for handling the timer used in millis()
+/* Initializes Timer0 for time tracking.
+   Must be called before using the timer functions.
+*/
 void timer0_init(void);
 
-// should be called from the general ISR to increment the timer
-// DOES NOT CLEAR PIR3bits.TMR0IF! This is the responsibility of the top level ISR.
+/* Handles Timer0 interrupt.
+   Should be called in the main ISR when Timer0 triggers an interrupt.
+   Note: Does not clear the interrupt flag `PIR3bits.TMR0IF`.
+*/
 void timer0_handle_interrupt(void);
 
-// Returns milliseconds since the timer was initialized
+/* Returns milliseconds since `timer0_init()` was called.
+   Useful for measuring elapsed time.
+   
+   Return:
+   uint32_t: Elapsed time in milliseconds.
+*/
 uint32_t millis(void);
 
 #endif /* ROCKETLIB_TIMER_H */
