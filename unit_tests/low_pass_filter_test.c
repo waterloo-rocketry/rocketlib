15af869f4078c5904be0d56a9b5b134c49ff839e
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2024-10-21 10:51:11 -0400

    Add rocketlib assert function

--- unit_tests/low_pass_filter_test.c
+++ unit_tests/low_pass_filter_test.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
+#include "common.h"
 #include "low_pass_filter.h"
-#include "unit_test.h"
 
 int main() {
-    status_t status = low_pass_filter_init();
+    w_status_t status = low_pass_filter_init();
 
-    rocketlib_assert(status == W_SUCCESS);
+    w_assert(status == W_SUCCESS);
 
     return 0;
 }
--- unit_tests/unit_test.c
+++ unit_tests/unit_test.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "unit_test.h"
+#include "common.h"
 
-void rocketlib_assert_fail(const char *file, int line, const char *statement) {
+void w_assert_fail(const char *file, int line, const char *statement) {
     printf("Assert Fail %s:%d %s\n", file, line, statement);
     exit(1);
 }
