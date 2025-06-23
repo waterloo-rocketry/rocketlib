f14431b9e62354ebc9abf9b0d76d3141ee7c0536
Andrew Wang <a488wang@uwaterloo.ca>
2025-06-23 05:50:31 -0400

    removed canlib dependancy etc

--- include/uart.h
+++ include/uart.h
@@ -6,7 +6,6 @@
 
 #include "common.h"
 
-
 /*
  * Initializes the UART1 module.
  *
@@ -24,7 +23,7 @@ w_status_t uart_init(uint32_t baud_rate, uint32_t fosc, bool enable_flow_control
  * waits until the transmit buffer is empty, then sends the byte.
  */
 void uart_transmit_byte(uint8_t byte);
-//void uart_transmit_buffer(uint8_t *tx, uint8_t len);
+// void uart_transmit_buffer(uint8_t *tx, uint8_t len);
 
 /*
  * Interrupt handler for UART1.
@@ -35,12 +34,12 @@ void uart_transmit_byte(uint8_t byte);
 void uart_interrupt_handler(void);
 
 /*
- * Callback function called when a byte is received.
+ * Internal callback function called when a byte is received via UART1.
  *
- * To use: define your own implementation of this function in your code.
- * If not defined by the user, the weak default will be used (does nothing).
+ * This is implemented in uart.c and currently echoes the received byte back.
+ * It is not intended to be overridden by user code due to MPLAB limitations.
  */
-void uart_rx_callback(uint8_t byte);  //override this, optional
+void uart_rx_callback(uint8_t byte);
 
 #endif /* ROCKETLIB_UART_H */
 
--- pic18f26k83/uart.c
+++ pic18f26k83/uart.c
@@ -1,27 +1,18 @@
-//#include <pic18f26k83.h> 
-#include <xc.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <xc.h>
 
-//#include "canlib.h" //remove -> no canlib dependancy 
-//#include "canlib/util/safe_ring_buffer.h"
 #include "common.h"
 #include "uart.h"
 
-// safe ring buffers for sending and receiving (DELETE)
-//static srb_ctx_t rx_buffer; 
-//static srb_ctx_t tx_buffer;
-
-// memory pools to use for those srbs. 100 is a completely arbitrary number
-//uint8_t rx_buffer_pool[100], tx_buffer_pool[100];
-
-//CALLBACK FXN HERE
-__attribute__ ((weak)) void uart_rx_callback (uint8_t byte) {
-    //user override fxn 
+void uart_rx_callback(uint8_t byte) {
+    uart_transmit_byte(byte); // echos back recieved byte
 }
 
 w_status_t uart_init(uint32_t baud_rate, uint32_t fosc, bool enable_flow_control) {
-    // only 12 or 48 MHz for pic
+    // make sure to Configure UART1 pins before running!!!
+
+    // only 12 or 48 MHz support for pic
     if (fosc != 12000000UL && fosc != 48000000UL) {
         return W_INVALID_PARAM;
     }
@@ -33,12 +24,12 @@ w_status_t uart_init(uint32_t baud_rate, uint32_t fosc, bool enable_flow_control
     U1CON0bits.BRGS = 0;
     uint32_t divisor = (U1CON0bits.BRGS == 1) ? 4 : 16;
 
-    // checks if baud rate param within range
+    // checks for valid baud rate
     if (baud_rate == 0 || baud_rate > (fosc / divisor)) {
         return W_INVALID_PARAM;
     }
 
-    // don't autodetect baudrate
+    // disable autodetect baudrate
     U1CON0bits.ABDEN = 0;
     // normal mode (8 bit, no parity, no 9th bit)
     U1CON0bits.MODE = 0;
@@ -55,107 +46,46 @@ w_status_t uart_init(uint32_t baud_rate, uint32_t fosc, bool enable_flow_control
     U1BRGH = (brg >> 8) & 0xFF;
     U1BRGL = brg & 0xFF;
 
-    // we are go for UART
+    // enable UART module
     U1CON1bits.ON = 1;
-
-    // initialize the rx and tx buffers DONT USE BUFFER
-    //srb_init(&rx_buffer, rx_buffer_pool, sizeof(rx_buffer_pool), sizeof(uint8_t));
-    //srb_init(&tx_buffer, tx_buffer_pool, sizeof(tx_buffer_pool), sizeof(uint8_t));
 
     // enable receive interrupt
     IPR3bits.U1RXIP = 1;
     PIE3bits.U1RXIE = 1;
-    // Do not enable transmit interrupt, that interrupt enable signals that
-    // there is data to be sent, which at init time is not true
+
+    // do not enable TX interrupt initially; only needed when there's queued data
 
     return W_SUCCESS;
 }
 
 void uart_transmit_byte(uint8_t byte) {
-    while (!PIR3bits.U1TXIF);  // blocking wait
+    while (!PIR3bits.U1TXIF)
+        ; // blocking wait
     U1TXB = byte;
 }
 
-//void uart_transmit_buffer(uint8_t *tx, uint8_t len) { //Implement transmit buffer without canlib dependency 
-//    //make into transmit byte fxn
-////Make this into a uart transmit byte function
-//    // just call uart_transmit_byte with each byte they want us to send
-//    while (len--) {
-//        //srb_push(&tx_buffer, tx);
-//        tx++;
-//    }
-//    // If the module isn't enabled, give it a byte to send and enable it
-//    if (PIE3bits.U1TXIE == 0) {
-//        //delete
-//        //srb_pop(&tx_buffer, &tx);
-//        U1TXB = tx;
-//        U1CON0bits.TXEN = 1;
-//        // also enable the interrupt for when it's ready to send more data
-//        PIE3bits.U1TXIE = 1;
-//    }
-//}
-//DELETE
-//bool uart_byte_available(void) {
-//    return !srb_is_empty(&rx_buffer);
-//}
-//DELETE
-//uint8_t uart_read_byte(void) {
- //   uint8_t rcv;
-  //  srb_pop(&rx_buffer, &rcv);
-  //  return rcv;
-//}
 void uart_interrupt_handler(void) {
-    //TX interrupt
+    // TX interrupt
     if (PIR3bits.U1TXIF) {
         PIR3bits.U1TXIF = 0;
     }
 
-    //RX interrupt
+    // RX interrupt
     if (PIR3bits.U1RXIF) {
         PIR3bits.U1RXIF = 0;
         uint8_t rcv = U1RXB;
-        uart_rx_callback(rcv);  // invoke user callback
+        uart_rx_callback(rcv); // invoke user callback
     }
 
-    //UART error interrupt
-    //ignored for now
+    // UART error interrupt
     if (PIR3bits.U1EIF) {
         PIR3bits.U1EIF = 0;
-        // Optionally handle framing/parity/overflow errors
+        // optionally handle framing/parity/overflow errors
     }
 
-    //global UART interrupt
+    // global UART interrupt
     if (PIR3bits.U1IF) {
         PIR3bits.U1IF = 0;
     }
 }
-//for receive, implement a callback option
-//void uart_interrupt_handler(void) {
-//    if (PIR3bits.U1TXIF) {
-//        // check if there are any bytes we still want to transmit
-//        if (!srb_is_empty(&tx_buffer)) {
-//            // if so, transmit them
-//            uint8_t tx;
-//            srb_pop(&tx_buffer, &tx);
-//            U1TXB = tx;
-//        } else {
-//            // If we have no data to send, disable this interrupt
-//            PIE3bits.U1TXIE = 0;
-//            // if not, disable the TX part of the uart module so that TXIF
-//            // isn't triggered again and so that we reenable the module on
-//            // the next call to uart_transmit_byte
-//            U1CON0bits.TXEN = 0;
-//        }
-//        PIR3bits.U1TXIF = 0;
-//    } else if (PIR3bits.U1RXIF) {
-//        // we received a byte, need to push into RX buffer and return
-//        uint8_t rcv = U1RXB;
-//        srb_push(&rx_buffer, &rcv);
-//        PIR3bits.U1RXIF = 0;
-//    } else if (PIR3bits.U1EIF) {
-//        // Some sort of error, ignore for now (TODO?)
-//        PIR3bits.U1EIF = 0;
-//    } else if (PIR3bits.U1IF) {
-//        PIR3bits.U1IF = 0;
-//    }
-//}
+
