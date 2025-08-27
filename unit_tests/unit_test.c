da7d63f3c388e0964b76ae435878085163810160
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-08-27 14:00:58 -0400

    Clang-format all code

--- unit_tests/unit_test.c
+++ unit_tests/unit_test.c
@@ -5,13 +5,13 @@
 #include "low_pass_filter_test.h"
 
 int main() {
-    test_low_pass_filter_init();
-    test_update_low_pass();
-    printf("\nAll tests passed!\n");
-    return 0;
+	test_low_pass_filter_init();
+	test_update_low_pass();
+	printf("\nAll tests passed!\n");
+	return 0;
 }
 
 void w_assert_fail(const char *file, int line, const char *statement) {
-    printf("Assert Fail %s:%d %s\n", file, line, statement);
-    exit(1);
+	printf("Assert Fail %s:%d %s\n", file, line, statement);
+	exit(1);
 }
