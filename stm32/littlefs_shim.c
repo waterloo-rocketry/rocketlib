9f2755f9bbff84e2bb1e5c7493539cd6655bce2f
Xukun Li <ximenalee5@gmail.com>
2025-11-24 21:23:12 -0500

    Debug

--- stm32/littlefs_shim.c
+++ stm32/littlefs_shim.c
@@ -6,11 +6,21 @@ uint32_t first_sector_offset = 0;
 
 int sd_read(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, void *buffer,
 			lfs_size_t size) {
+
 	uint32_t timeout_ms = 50U;
 	uint32_t block_addr = block + first_sector_offset;
-	uint32_t num_blocks = (size + c->block_size - 1) / c->block_size;
+	//uint32_t num_blocks = (size + c->block_size - 1) / c->block_size;
+	uint32_t num_blocks = 1;
+	if((off+size)>512){
+		for(;;){}
+	}
+	uint8_t blockbuf[512];
 	HAL_StatusTypeDef hal =
-		HAL_SD_ReadBlocks(lfs_shim_hsd, (uint8_t *)buffer, block_addr, num_blocks, timeout_ms);
+		HAL_SD_ReadBlocks(lfs_shim_hsd, blockbuf, block_addr, num_blocks, timeout_ms);
+	uint8_t* buf = (uint8_t*)buffer;
+	for(size_t i = 0; i < size; i++) {
+		buf[i] = blockbuf[i+off];
+	}
 	if (hal != HAL_OK) {
 		return -1; // LFS_ERR_IO
 	}
