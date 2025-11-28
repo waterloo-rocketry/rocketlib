d7be31d127d240fdfd4cbcffd65f218c7fddb937
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2025-11-28 18:15:59 -0500

    Add stm32h7 folder to clang-format flow

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -x
-${CLANG_FORMAT:-clang-format} -i common/*.c include/*.h pic18f26k83/*.c tests/*.cpp tests/*.hpp
+${CLANG_FORMAT:-clang-format} -i common/*.c include/*.h pic18f26k83/*.c stm32h7/*.c tests/*.cpp tests/*.hpp
